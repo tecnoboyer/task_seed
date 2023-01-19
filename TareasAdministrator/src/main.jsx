016035eb2502a5ebf931fba2c674d2a19f6a3847
tecnoboyer <ingenieroboyer@gmail.com>
2023-01-18 20:29:36 -0500

    Esqueleto

---
+++ TareasAdministrator/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {Cascara} from './components/Cascara'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cascara />
+  </React.StrictMode>,
+)
